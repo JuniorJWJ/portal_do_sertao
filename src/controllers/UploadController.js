1b52e83fae564f6c86cc2ef7da62d1512d7d9cf6
JuniorJWJ <54831275+JuniorJWJ@users.noreply.github.com>
2021-10-13 14:14:01 -0300

    gitignore/keep, novas colunas

--- src/controllers/UploadController.js
+++
@@ -1,9 +0,0 @@
-// const Upload = require('../model/autor')
-
-const { create } = require("./autorController")
-
-module.exports = {
-    async get(req,res){
-        return res.render("upload")
-    }
-}
